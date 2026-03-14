b94debcf021b5cbe9ff502ab1792a8f226b93471
BSY3510 <baeksy3510@gmail.com>
2026-03-15 01:28:47 +0900

    initial setting

---
+++ src/main/java/com/bsy/shared_notepad/SharedNotepadApplication.java
@@ -0,0 +1,13 @@
+package com.bsy.shared_notepad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SharedNotepadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SharedNotepadApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bsy/shared_notepad/SharedNotepadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bsy.shared_notepad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SharedNotepadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
